1f52e705401df19d09b7c37bf27042f0c0376ba9
Eureka Zheng <1210367416@qq.com>
2018-01-18 15:29:39 -0500

    hw.m update

--- hw2/hw.m
+++ hw2/hw.m
@@ -1,20 +1,24 @@
 % Linear equations system solver
 % Shaoming Zheng, Evan White, Kevin Pietz
 
-disp('This is a linear equations system solver')
+disp('This is a linear equations system solver');
 reply = 'y';
 while reply == 'y'
-    datafile = input('Please enter the name of the data file that stores the matrix [A|b]: ', 's');
+    datafile = input('Enter the name of the data file: ', 's');
+    if exist(datafile, 'file') == 0
+        disp('Error: file not exist!');
+        continue;
+    end
     mat = load(datafile);
     [rows, cols] = size(mat);
     if (rows > cols)
         [cols, rows] = deal(rows, cols);
-        mat = mat.';
+        mat = mat';
     end
     A = mat(:, 1:cols-1);
     if cols ~= rows + 1
         disp('Error: inconsistent dimensions!');
-    elseif inv(A) == 0
+    elseif det(A) == 0
         disp('Error: unsolvable matrix!');
     else
         b = mat(:, cols);
