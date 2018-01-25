42375163241194c530914f645740e0a29413dcdc
unknown <etw10@pitt.edu>
2018-01-25 18:33:54 -0500

    finished the script, all requirements met, add comments

--- Assignment 3/R04_hw3.m
+++ Assignment 3/R04_hw3.m
@@ -1,10 +1,3 @@
-%used for loops for many things because assignment requires manual
-%calculation of most data outputs
-
-%Still Need:
-%the histogram with both sets of data on it
-%might not need to display min and max, just calculate them
-
 %Evan White, Eureka Zheng, Kevin Pietz
 %Created on 1/24/2018
 %Last Edited on 1/25/2018
@@ -31,117 +24,177 @@
 %totaldiff: used to sum point-mean total for variance calculation
 %varofdata: the variance of the data
 %standdev: the standard deviation of the data
+%numbins: the number of bins
+%binbounds: an array with all the bounds for the bins
+%currentcheck: the number currently being check in the bin count calc
+%bincounts: an arry with all the counts for each bin
+%xlab: variable for x label
+%ylab: variable for y label
+%placetext: variable for use in gtext
 
 clear;
 clc;
-x=1;
+again='y';
 
-for x=1:2
-    %for loop for running script for both data sets
-    
-    
-    if x==1
-        xsuffix='1st';
-    else
-        xsuffix='2nd';
-    end
-    
-    %userdata will be the variable for whatever data is input
-    outputtext=['Please enter the name of your ',xsuffix,' datafile: '];
-    %the text has the 1st or second from xsuffix to make more userfriendly
-    userdata=input(outputtext,'s');
-    
-    
-    %making sure it actually exists, if not, they have to reenter the name
-    while exist(userdata,'file')==0
-        outputtext=['Error! Please reenter the name of your ',xsuffix,' datafile: '];
+while again=='y'
+    x=1;
+    for x=1:2
+        %for loop for running script for both data sets
+        
+        
+        if x==1
+            xsuffix='1st';
+        else
+            xsuffix='2nd';
+        end
+        
+        %userdata will be the variable for whatever data is input
+        outputtext=['Please enter the name of your ',xsuffix,' datafile: '];
+        %the text has the 1st or second from xsuffix to make more userfriendly
         userdata=input(outputtext,'s');
-    end
-    
-    %loading the data into the variable userdata
-    userdata=load(userdata);
-    
-    %finding the number datapoints and putting into "numdata"
-    numdata=length(userdata);
-    
-    %for loop to get the total of all the data
-    totalofdata=0;
-    for n=1:numdata
-        totalofdata=userdata(n)+totalofdata;
-    end
-    
-    
-    %getting mean of data using totalofdata divided by numdata
-    meanofdata=totalofdata/numdata;
-    disp('The Mean of the Data is: ');
-    disp(meanofdata);
-    
-    %min of data is first data point of sorted data
-    currmin=userdata(1);
-    %set current min to the first data point to start
-    n=0;
-    %set n back to 0 for the for loop
-    for n=2:numdata
-        if userdata(n)<currmin
-            currmin=userdata(n);
+        
+        
+        %making sure it actually exists, if not, they have to reenter the name
+        while exist(userdata,'file')==0
+            outputtext=['Error! Please reenter the name of your ',xsuffix,' datafile: '];
+            userdata=input(outputtext,'s');
         end
-        %if userdata at index n is less than the current min, set the current
-        %min equal to that userdata point
-    end
-    minofdata=currmin;
-    disp('The Min of the Data is: ');
-    disp(minofdata);
-    
-    %setting n back to 0 and currmax to the first data point
-    n=0;
-    currmax=userdata(1);
-    for n=2:numdata
-        if userdata(n)>currmax
-            currmax=userdata(n);
+        
+        %loading the data into the variable userdata
+        userdata=load(userdata);
+        
+        %finding the number datapoints and putting into "numdata"
+        numdata=length(userdata);
+        
+        %for loop to get the total of all the data
+        totalofdata=0;
+        for n=1:numdata
+            totalofdata=userdata(n)+totalofdata;
         end
+        
+        numbins=input('Please Enter the Number of Bins: ');
+        while isnumeric(numbins)==0||numbins<1
+            numbins=input('Error! Please Enter the Number of Bins: ');
+        end
+        
+        
+        %getting mean of data using totalofdata divided by numdata
+        meanofdata=totalofdata/numdata;
+        disp('The Mean of the Data is: ');
+        disp(meanofdata);
+        
+        %min of data is first data point of sorted data
+        currmin=userdata(1);
+        %set current min to the first data point to start
+        
+        for n=2:numdata
+            if userdata(n)<currmin
+                currmin=userdata(n);
+            end
+            %if userdata at index n is less than the current min, set the current
+            %min equal to that userdata point
+        end
+        minofdata=currmin;
+        
+        
+        %setting n back to 0 and currmax to the first data point
+        
+        currmax=userdata(1);
+        for n=2:numdata
+            if userdata(n)>currmax
+                currmax=userdata(n);
+            end
+        end
+        %just like with min, every data point is checked, and this time if its
+        %greater than the last, it is set as the current max
+        maxofdata=currmax;
+        
+        
+        %sorting the data now to find the variance
+        sorteddata=sort(userdata);
+        %if there is an even number of data points:
+        if mod(numdata,2)==0
+            point1=numdata/2;
+            point2=(numdata/2)+1;
+            %median is the average of the halfway point and the 1 after that in an
+            %even number of data points
+            medofdata=(sorteddata(point1)+sorteddata(point2))/2;
+        else
+            %the point is halfway through the data for odd num of variables
+            point=ceil(numdata/2);
+            %median is the data point at halfway in the sorted data for odd num of
+            %variables
+            medofdata=sorteddata(point);
+        end
+        disp('The Median of the Data is: ');
+        disp(medofdata);
+        
+        %finding the variance and displaying it
+        n=0;
+        totaldiff=0;
+        for n=1:numdata
+            %totaldiff is current total + sorted(n)-mean squared
+            totaldiff=totaldiff+((sorteddata(n)-meanofdata)^2);
+        end
+        %numdata-1 is to remain consistent with the variance equation
+        varofdata=totaldiff/(numdata-1);
+        disp('The Variance of the Data is: ');
+        disp(varofdata);
+        
+        %standard deviation is square root of variance
+        standdev=sqrt(varofdata);
+        disp('The Standard Deviation of the Data is: ');
+        disp(standdev);
+        
+        
+        if x==1
+            figure %so a new figure is made every time the user restarts
+            subplot(2,1,x)
+            hist(sorteddata,numbins);
+            title('Using the HIST Command');
+            xlab=input('Please type your x label: ','s');
+            xlabel(xlab);
+            ylab=input('Please type your y label: ','s');
+            ylabel(ylab);
+            placetext=input('Please type your text to be placed on graph: ','s');
+            gtext(placetext);
+        else
+            range=sorteddata(numdata)-sorteddata(1);
+            delta=range/numbins;
+            binbounds=zeros(1,numbins);
+            bincounts=zeros(1,numbins);
+            binbounds(1)=minofdata;
+            for n=2:numbins+1
+                binbounds(n)=binbounds(n-1)+delta;
+            end
+            for n=1:numdata 
+                currentcheck=sorteddata(n); %check every number from 1 to number of datapoints
+                for m=numbins:-1:1
+                    if currentcheck>=binbounds(m) %if current num being check is greater or equal to binbounds at m, add a bincount at m
+                        bincounts(m)=bincounts(m)+1;
+                        break;
+                    end
+                end
+            end
+            subplot(2,1,x)
+            bar(binbounds(1:numbins),bincounts);
+             title('Using the bar Command');
+            xlab=input('Please type your x label: ','s');
+            xlabel(xlab);
+            ylab=input('Please type your y label: ','s');
+            ylabel(ylab);
+            placetext=input('Please type your text to be placed on graph: ','s');
+            gtext(placetext);
+        end
+        
     end
-    %just like with min, every data point is checked, and this time if its
-    %greater than the last, it is set as the current max
-    maxofdata=currmax;
-    disp('The Max of the Data is: ');
-    disp(maxofdata);
+    again=input('Would you like to run again? (y/n)  ','s');
+    %the if, else if, else for errors on inputing the users choice
+    if again=='y'
     
-    %sorting the data now to find the variance
-    sorteddata=sort(userdata);
-    %if there is an even number of data points:
-    if mod(numdata,2)==0
-        point1=numdata/2;
-        point2=(numdata/2)+1;
-        %median is the average of the halfway point and the 1 after that in an
-        %even number of data points
-        medofdata=(sorteddata(point1)+sorteddata(point2))/2;
+    elseif again=='n'
+       disp('Goodbye!'); 
     else
-        %the point is halfway through the data for odd num of variables
-        point=ceil(numdata/2);
-        %median is the data point at halfway in the sorted data for odd num of
-        %variables
-        medofdata=sorteddata(point);
+        again=input('Error! Would you like to run again? (y/n)  ','s');
     end
-    disp('The Median of the Data is: ');
-    disp(medofdata);
-    
-    %finding the variance and displaying it
-    n=0;
-    totaldiff=0;
-    for n=1:numdata
-        %totaldiff is current total + sorted(n)-mean squared
-        totaldiff=totaldiff+((sorteddata(n)-meanofdata)^2);
-    end
-    %numdata-1 is to remain consistent with the variance equation
-    varofdata=totaldiff/(numdata-1);
-    disp('The Variance of the Data is: ');
-    disp(varofdata);
-    
-    %standard deviation is square root of variance
-    standdev=sqrt(varofdata);
-    disp('The Standard Deviation of the Data is: ');
-    disp(standdev);
-    
 end
-
-
