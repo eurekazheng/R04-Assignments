29ec78f6861bbeadf81361c7584c008ec2fe3d2b
Kevinpietz <35549224+Kevinpietz@users.noreply.github.com>
2018-02-10 17:52:08 -0500

    Add files via upload

---
+++ Assignment 7/addequation.m
@@ -0,0 +1,17 @@
+function var3=addequation(coeffs,plottype)
+    % Function created by Kevin Pietz on 2.10.2018
+    % Purpose to add equation to graph
+    
+    if plottype==1
+        eqn=strcat(['y =',' ',num2str(coeffs(1)),'x +',' ',num2str(coeffs(2))]);
+        gtext(eqn)
+    elseif plottype==4
+        eqn=strcat(['y =',' ',num2str(coeffs(1)),'e^{','-',num2str(coeffs(2)),'x','}']);
+        gtext(eqn)
+    elseif plottype==5
+        eqn=strcat(['y =',' ',num2str(coeffs(1)),'x^{',num2str(coeffs(2)),'}']);
+        gtext(eqn)
+    end
+    
+    
+end
