c239ab5657cf8864f94cbf7413639d6fc56b73be
Kevinpietz <35549224+Kevinpietz@users.noreply.github.com>
2018-02-11 15:02:38 -0500

    Add files via upload

--- Assignment 7/findy.m
+++ Assignment 7/findy.m
@@ -13,13 +13,14 @@ function findy(x,y)
             
         case 2   % Polynomial find
             [u,v,coeffs]=PolynomialFit(x,y);
-            
+            eqn=0;
             degree=length(coeffs)-1;
             for n=1:degree
-             eqn=(coeffs(degree+1-n)).*x.^(n)+eqn;
+             eqn=(coeffs(degree+1-n)).*x_est.^(n)+eqn;
             end
             eqn=eqn+coeffs(degree+1);
-            y_est=eqn(x_est);
+            y_est=eqn;
+            
         case 3   % Spline find
             
         case 4   % interp(lin) find
@@ -28,5 +29,6 @@ function findy(x,y)
             
         case 6   % interp(spline) find
     end
+    
     disp(['The x value, ',num2str(x_est),', corresponds to the y value, ',num2str(y_est)]);
 end
