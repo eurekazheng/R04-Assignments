b5137d75d64a2bc7e5a4c4b17e778f74b27e9987
Kevinpietz <35549224+Kevinpietz@users.noreply.github.com>
2018-02-17 14:50:37 -0500

    Add files via upload

--- Assignment 9/R04_Assn9.m
+++ Assignment 9/R04_Assn9.m
@@ -11,11 +11,11 @@ f=menu('What type of function would you like to use?','File','Anonymous');
 
 switch f
     case 1
-        funf=input('Please enter the filename: ','s');
+        funf=str2func(['@',input('Please enter the filename: ','s')]);
     case 2
         funf=input('Enter the function, no need for the ''@'' symbol: ','s');
-        funf=strcat(['@(x)',funf]);
-        funf=eval(funf);
+        funf=strcat(['@',funf]);
+        
 end
 
 % Prompt for analysis type, perform request
