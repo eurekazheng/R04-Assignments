44cb09f121d608603f8232bed609489c4889e423
Kevinpietz <35549224+Kevinpietz@users.noreply.github.com>
2018-02-17 15:27:30 -0500

    Add files via upload

---
+++ Assignment 9/maxoffunf.m
@@ -0,0 +1,16 @@
+function maxoffunf(funf)
+notfunf=-funf;
+
+xmin=input('Enter the left side of the range in which you want to find the maximum value: ');
+xmax=input('Enter the right side of the range in which you want to find the maximum value: ');
+
+if xmin>xmax
+    tempxmin=xmax;
+    xmax=xmin;
+    xmin=tempxmin;
+end
+
+maximumx=fminbnd(notfunf,xmin,xmax);
+maximumy=funf(maximumx);
+disp('The location of your maximum is ',num2str(maximumx),',',num2str(maximumy));
+end
