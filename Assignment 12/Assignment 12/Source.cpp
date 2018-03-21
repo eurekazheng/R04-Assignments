7585e9706d79070990df634375f4441992916f69
unknown <etw10@pitt.edu>
2018-03-21 16:49:17 -0400

    Maybe done with part a?

--- Assignment 12/Assignment 12/Source.cpp
+++ Assignment 12/Assignment 12/Source.cpp
@@ -9,10 +9,12 @@
 int partA(void) {
 	char again;
 	int n, i;
-	float t,output=0;
+	float t,output;
 	do {
 		//setting value for summation
-		i = 0;
+		n = 0;
+		t = 0;
+		output = 0;
 
 		//getting value for t
 		printf("Input a value for t:\n");
@@ -25,12 +27,12 @@ int partA(void) {
 		scanf(" %d", &n);
 
 		//summing up V given inputs
-		while (i <= n) {
-			output = (1 / (pow(2*i-1,2)))*cos(((2 * i - 1)*3.14159265*t) / 3)+output;
-			i++;
+		for (i = 1; i <= n; i = i + 1) {
+			output = (1/((2*i-1)*(2*i-1)))*cos(((2*i-1)*(3.14159265*t))/3)+output;
 		}
+
 		//part of V outside of sum
-		output = output * ((3 / 2) - (12 / (3.14159265*3.14159265)));
+		output = ((3 / 2) - (12 / (3.14159265*3.14159265)))*output;
 		
 		//displaying results
 		printf("n = %d\n", n);
@@ -59,9 +61,27 @@ int partC(void) {
 
 //main function
 int main(void) {
-	partA();
-	partB();
-	partC();
+	int choice = 10;
+	char againm;
+	do {
+		printf("Do you want to try part 1, 2, or 3?: \n");
+		scanf(" %d", &choice);
+		switch (choice) {
+		case 1:
+			partA();
+			break;
+		case 2:
+			partB();
+			break;
+		case 3:
+			partC();
+			break;
+		}
+		printf("Do you want to do the whol program again? (y/n):\n");
+		scanf(" %c", &againm);
+	} while (againm == 'y');
+	
+	printf("\nGoodbye!\n\n");
 	system("pause");
 }
 
