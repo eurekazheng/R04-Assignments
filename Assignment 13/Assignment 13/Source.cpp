2a50cc5b1b7db69b0bdfb4e5d7f98ece1240729c
etw10 <etw10@pitt.edu>
2018-03-28 15:20:27 -0400

    finish assignment 13

--- Assignment 13/Assignment 13/Source.cpp
+++ Assignment 13/Assignment 13/Source.cpp
@@ -4,10 +4,103 @@
 #include <math.h>
 #include <string.h>
 
+/*
+Kevin Pietz, Eureka Zheng, Evan White
+Engineering 0012 T/Th 2:00-4:00
+Instructor: Mandala
+Date: 3/29/2018
+*/
+
+
+
+//function for finding month of an inputted day (input #/365, output #/12)
+int datemonth(int n) {
+	int month, day;
+	if (n <= 31) {
+		month = 1;
+	}
+	else if (n > 31 && n <= 59) {
+		month = 2;
+	}
+	else if (n > 59 && n <= 90) {
+		month = 3;
+	}
+	else if (n > 90 && n <= 120) {
+		month = 4;
+	}
+	else if (n > 120 && n <= 151) {
+		month = 5;
+	}
+	else if (n > 151 && n <= 181) {
+		month = 6;
+	}
+	else if (n > 181 && n <= 212) {
+		month = 7;
+	}
+	else if (n > 212 && n <= 243) {
+		month = 8;
+	}
+	else if (n > 243 && n <= 273) {
+		month = 9;
+	}
+	else if (n > 273 && n <= 304) {
+		month = 10;
+	}
+	else if (n > 304 && n <= 334) {
+		month = 11;
+	}
+	else if (n > 334 && n <= 365) {
+		month = 12;
+	}
+	return(month);
+}
+
+//function for finding day of an inputted day (from #/365 to # within a month)
+int dateday(int n) {
+	int month, day;
+	if (n <= 31) {
+		day = n;
+	}
+	else if (n > 31 && n <= 59) {
+		day = n - 31;
+	}
+	else if (n > 59 && n <= 90) {
+		day = n - 59;
+	}
+	else if (n > 90 && n <= 120) {
+		day = n - 90;
+	}
+	else if (n > 120 && n <= 151) {
+		day = n - 120;
+	}
+	else if (n > 151 && n <= 181) {
+		day = n - 151;
+	}
+	else if (n > 181 && n <= 212) {
+		day = n - 181;
+	}
+	else if (n > 212 && n <= 243) {
+		day = n - 212;
+	}
+	else if (n > 243 && n <= 273) {
+		day = n - 243;
+	}
+	else if (n > 273 && n <= 304) {
+		day = n - 273;
+	}
+	else if (n > 304 && n <= 334) {
+		day = n - 304;
+	}
+	else if (n > 334 && n <= 365) {
+		day = n - 334;
+	}
+	return(day);
+}
+
 
 int main(void) {
-	printf("ENGR0012\nR04\nMandala 2 PM\nEvan White, Kevin Pietz, Eureka Zheng\n\n"); // header funciton
-
+	printf("ENGR0012\nR04\nMandala 2 PM\nEvan White, Kevin Pietz, Eureka Zheng\n\n"); // displaying header
+	
 	FILE *fname;
 	fname = fopen("1930_2001.csv","r");
 	
@@ -65,43 +158,53 @@ int main(void) {
 
 	//min and max stuff
 	//max
+	int max1day, max2day, min1day, min2day;
 
 	int max1 = firsttemps[1];
 	int i1;
 	for (i1 = 2; i1 <= 365; i1++) {
-		if (max1 < firsttemps[i1])
+		if (max1 < firsttemps[i1]) {
 			max1 = firsttemps[i1];
+			max1day = i1;
+		}
 	}
-	printf("The maximum temperature in 1930 was %d\n", max1);
+	printf("The maximum temperature in 1930 was %d on %d/%d\n", max1, datemonth(max1day),dateday(max1day));
 
 	int max2 = secondtemps[1];
 	int i2;
 	for (i2 = 2; i2 <= 365; i2++) {
-		if (max2 < secondtemps[i2])
+		if (max2 < secondtemps[i2]) {
 			max2 = secondtemps[i2];
+			max2day = i2;
+		}
+
 	}
-	printf("The maximum temperature in 2001 was %d\n\n", max2);
+	printf("The maximum temperature in 2001 was %d on %d/%d\n\n", max2, datemonth(max2day), dateday(max2day));
 
 	// min
 
 	int min1 = firsttemps[0];
 	int n1;
 	for (n1 = 1; n1 < 364; n1++) {
-		if (min1 > firsttemps[n1])
+		if (min1 > firsttemps[n1]) {
 			min1 = firsttemps[n1];
+			min1day = n1;
+		}
 	}
-	printf("The minimum temperature in 1930 was %d\n", min1);
+	printf("The minimum temperature in 1930 was %d on %d/%d\n", min1, datemonth(min1day), dateday(min1day));
 
 	int min2 = secondtemps[1];
 	int n2;
 	for (n2 = 1; n2 < 364; n2++) {
-		if (min2 > secondtemps[n2])
+		if (min2 > secondtemps[n2]) {
 			min2 = secondtemps[n2];
+			min2day = n2;
+		}
 	}
-	printf("The minimum temperature in 2001 was %d\n\n\n", min2);
+	printf("The minimum temperature in 2001 was %d on %d/%d\n\n\n", min2, datemonth(min2day), dateday(min2day));
 	
 
-	//differences
+	//calculating differences
 	int diff[365];
 	int maxDiff = 0xf0000000;
 	int minDiff = 0x7fffffff;
@@ -114,69 +217,21 @@ int main(void) {
 		mini = diff[i] < minDiff ? i : mini;
 		minDiff = diff[i] < minDiff ? diff[i] : minDiff;
 	}
-	mini++;
+	//raise maxi and mini by 1 each because C arrays start at 0, not 1
 	maxi++;
-	int mindday, maxdday;
-	int monthd;
-	if (maxi <= 31) {
-		monthd = 1;
-		maxdday = maxi;
-	} else if(maxi > 31 && maxi <= 59) {
-		monthd = 2;
-		maxdday = maxi - 31;
-	}
-	else if (maxi > 59 && maxi <= 90) {
-		monthd = 3;
-		maxdday = maxi - 59;
-	}
-	else if (maxi > 90 && maxi <= 120) {
-		monthd = 4;
-		maxdday = maxi - 90;
-	}
-	else if (maxi > 120 && maxi <= 151) {
-		monthd = 5;
-		maxdday = maxi - 120;
-	}
-	else if (maxi > 151 && maxi <= 181) {
-		monthd = 6;
-		maxdday = maxi - 151;
-	}
-	else if (maxi > 181 && maxi <= 212) {
-		monthd = 7;
-		maxdday = maxi - 181;
-	}
-	else if (maxi > 212 && maxi <= 243) {
-		monthd = 8;
-		maxdday = maxi - 212;
-	}
-	else if (maxi > 243 && maxi <= 273) {
-		monthd = 9;
-		maxdday = maxi - 243;
-	}
-	else if (maxi > 273 && maxi <= 304) {
-		monthd = 10;
-		maxdday = maxi - 273;
-	}
-	else if (maxi > 304 && maxi <= 334) {
-		monthd = 11;
-		maxdday = maxi - 304;
-	}
-	else if (maxi > 334 && maxi <= 365) {
-		monthd = 12;
-		maxdday = maxi - 334;
-	}
+	mini++;
 
-	printf("Max Difference: %d on %d/%d\n", maxDiff,monthd,maxdday);
-	printf("Min Difference: %d on day: %d\n\n", minDiff, mini);
+	//printing date of maximum difference
+	printf("Max Difference: %d on %d/%d\n", maxDiff,datemonth(maxi),dateday(maxi));
+	printf("Min Difference: %d on %d/%d\n\n", minDiff, datemonth(mini),dateday(mini));
 
-
-	//writing results to a dat file
+	//writing results to a .dat file
 	FILE *fname2;
 	fname2 = fopen("results.dat", "w");
 	fprintf(fname2,"Mean in 1930 = %.2f\nMean in 2001 = %.2f\nStandard Deviation in 1930: %.2f\nStandard Deviation in 2001: %.2f\n\n\n", firstmean, secondmean, stdev1, stdev2);
-	fprintf(fname2, "Max in 1930 = %d\nMax in 2001 = %d\nMin in 1930 = %d\nMin in 2001 = %d\n\n", max1, max2, min1, min2);
+	fprintf(fname2, "Max in 1930 = %d on %d/%d\nMax in 2001 = %d on %d/%d\nMin in 1930 = %d on %d/%d\nMin in 2001 = %d on %d/%d\n\n", max1,datemonth(max1day),dateday(max1day), max2,datemonth(max2day),dateday(max2day), min1,datemonth(min1day),dateday(min1day), min2,datemonth(min2day),dateday(min2day));
 	fprintf(fname2, "\nCalculating Difference with 1930 Temps Subtracted from 2001 Temps:\n");
-	fprintf(fname2, "Max difference = %d on %d/%d\nMin difference = %d\n", maxDiff,monthd,maxdday, minDiff);
+	fprintf(fname2, "Max difference = %d on %d/%d\nMin difference = %d on %d/%d\n", maxDiff,datemonth(maxi),dateday(maxi), minDiff,datemonth(mini),dateday(mini));
 	fclose(fname2);
 
 
