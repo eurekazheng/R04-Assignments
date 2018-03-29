81e2f94266c808539a89903cd3e9dd4e453fdb29
etw10 <etw10@pitt.edu>
2018-03-29 16:12:27 -0400

    Finish Assign 14

--- Assignment 14/Assignment 14/Source.cpp
+++ Assignment 14/Assignment 14/Source.cpp
@@ -1,33 +1,35 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
 
-//still need:
-//Display: ERROR! when errors happen
-//make the printfs better and not shit looking
+//Evan White, Kevin Pietz, Eureka Zheng
+//R04
+//Assignment 14
+//ENGR0012 T/Th 2-4PM
+//Instructor: Mandala
 
 //prototype functions
 void header(void);
 float numberinput(int n);
 int opchoice(void);
-void mathops(int optype,float num1, float num2);
-void txtops(int optype, float num1, float num2);
+float mathops(int optype,float num1, float num2);
+void txtops(float output,int optype, float num1, float num2);
 
 
 //main function
 int main(void) {
 	header();
 
+	//initializing variables
 	float num1,num2;
 	int optype;
 	char again='y',againnum;
 
+	//outer while loop is for user wants to continue but with different numbers
 	while (again=='y') {
 
 
-		//getting the users numbers
+		//getting the users numbers using func 1
 		int n;
 		for (n = 1; n <= 2; n++) {
 			if (n == 1) {
@@ -42,10 +44,17 @@ int main(void) {
 		againnum = 'y';
 
 		//operations section
+		//inner while loop is if user wants to continue with same numbers
 		while (againnum=='y') {
-			//getting op type by running
+			float output;
+			//getting optype from func 3
 			optype = opchoice();
-			mathops(optype, num1, num2);
+			//getting output from func 4
+			output=mathops(optype, num1, num2);
+			//displaying results and logging to .txt file with func 5
+			txtops(output,optype,num1,num2);
+
+
 
 			//checking if user wants to run again with same numbers
 
@@ -90,60 +99,58 @@ float numberinput(int n) {
 int opchoice(void) {
 	int choice=0;
 
+
+	printf("Please enter the operation to perform on your numbers:\n   1) Addition\n   2) Subtraction\n   3) Multiplication\n   4) Division\n");
+	scanf(" %d", &choice);
 	while (choice > 4 || choice < 1) {
-		printf("Please enter the operation to perform on your numbers:\n   1) Addition\n   2) Subtraction\n   3) Multiplication\n   4) Division\n");
-		scanf(" %d",&choice);
+		printf("ERROR! ENTER ONE OF THE OPTIONS!\n Please enter the operation you wish to perform on your numbers:\n   1) Addition\n   2) Subtraction\n   3) Multiplication\n   4) Division\n");
+		scanf(" %d", &choice);
 	}
 
 	return(choice);
 }
 
 //function 4 -- math operations
-void mathops(int optype, float num1, float num2) {
+float mathops(int optype, float num1, float num2) {
 	float output;
-	printf("Using numbers: num1 = %f and num2 = %f\n", num1, num2);
 	switch (optype) {
 	case 1:
 		output = num1 + num2;
-		printf("The Addition: %f + %f = %f\n\n", num1, num2, output);
 		break;
 	case 2:
 		output = num1 - num2;
-		printf("The Subtraction: %f - %f = %f\n\n", num1, num2, output);
 		break;
 	case 3:
 		output = num1 * num2;
-		printf("The Multipication: %f * %f = %f\n\n", num1, num2, output);
 		break;
 	case 4:
 		output = num1 / num2;
-		printf("The Subtraction: %f / %f = %f\n\n", num1, num2, output);
 		break;
 	}
-	txtops(optype, num1, num2);
+	return(output);
 }
 
 //function 5 -- putting results in a text file
-void txtops(int optype, float num1, float num2) {
+//numbers displaying as %.2f to look like sample output
+void txtops(float output,int optype, float num1, float num2) {
 	FILE *fname;
 	fname = fopen("results.txt", "a");
-	float output;
 	switch (optype) {
 	case 1:
-		output = num1 + num2;
-		fprintf(fname,"The Addition: %f + %f = %f\n", num1, num2, output);
+		printf("The Addition: %.2f + %.2f = %.2f\n", num1, num2, output);
+		fprintf(fname,"The Addition: %.2f + %.2f = %.2f\n", num1, num2, output);
 		break;
 	case 2:
-		output = num1 - num2;
-		fprintf(fname,"The Subtraction: %f - %f = %f\n", num1, num2, output);
+		printf("The Subtraction: %.2f - %.2f = %.2f\n", num1, num2, output);
+		fprintf(fname,"The Subtraction: %.2f - %.2f = %.2f\n", num1, num2, output);
 		break;
 	case 3:
-		output = num1 * num2;
-		fprintf(fname,"The Multipication: %f * %f = %f\n", num1, num2, output);
+		printf("The Multipication: %.2f * %.2f = %.2f\n", num1, num2, output);
+		fprintf(fname,"The Multipication: %.2f * %.2f = %.2f\n", num1, num2, output);
 		break;
 	case 4:
-		output = num1 / num2;
-		fprintf(fname,"The Subtraction: %f / %f = %f\n", num1, num2, output);
+		printf("The Division: %.2f / %.2f = %.2f\n", num1, num2, output);
+		fprintf(fname,"The Division: %.2f / %.2f = %.2f\n", num1, num2, output);
 		break;
 	}
 	fclose(fname);
