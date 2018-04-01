fee585ca397898f191f4979deb20e0959c1dc446
etw10 <etw10@pitt.edu>
2018-04-01 17:23:05 -0400

    added deposit function

--- Assignment 15/Assignment 15/R04_HW15_2PM.cpp
+++ Assignment 15/Assignment 15/R04_HW15_2PM.cpp
@@ -16,6 +16,9 @@ struct userbalance {
 //prototype functions
 void header(void);
 int menu(void);
+void dispbalance(double checkingb, double savingsb);
+double deposit(double currentbal);
+double withdraw(double currentbal);
 
 int main(void) {
 	struct userbalance user1;
@@ -41,8 +44,9 @@ int main(void) {
 		printf("Incorrect Password! You have %d attempts remaining. Please Renter your Password: ",attempts);
 		scanf(" %d", &pentry);	
 	}
+	//If user gets password right on last attempt, attempts would equal 1 so the while loop wouldnt run again, so it is raised by one so the main loop will run
 	if (pentry == pass && attempts == 1)
-		attempts = 2;
+		attempts++;
 
 	//data stored in balance file as checking,savings
 	FILE *data;
@@ -51,18 +55,46 @@ int main(void) {
 	fclose(data);
 
 	char again = 'y';
+	int uchoice,accountc;
 	//script with functions in it if user successfully logs in
 	while (again == 'y'&&attempts > 1) {
 
-		//menu function
-		//switch case
-		//display balances
-		//deposit function
-		//withdraw function
-		//exit
+		uchoice = menu();
+		switch (uchoice) {
+		case 1: //displaying balance
+			dispbalance(user1.checkingb,user1.savingsb);
+			break;
+		case 2: //deposit
+			//checking which account user will deposit to
+			printf("Which account will you deposit to?\n1) Checking\n2) Savings");
+			scanf(" %d", &accountc);
+			//error checking their account input
+			while (accountc != 1 && accountc != 2) {
+				printf("Error! Please enter a 1 or 2!\nWhich account will you deposit to?\n1) Checking\n2) Savings\n");
+				scanf(" %d", &accountc);
+			}
+			//based on their account, the inputs to the deposit function are switched
+			switch (accountc) {
+			case 1:
+				user1.checkingb=deposit(user1.checkingb);
+				break;
+			case 2:
+				user1.savingsb = deposit(user1.savingsb);
+				break;
+			}
+			//printing out the new values
+			printf("Your new balances are:\n");
+			dispbalance(user1.checkingb,user1.savingsb);
+			break;
+		case 3: //withdraw
+			printf("withdraw \n");
+			break;
+		case 4: //exit
+			break;
+		};
 
-		//displaying balance
-		printf("\n\nChecking balance = $%.2lf\nSavings balance = $%.2lf", user1.checkingb, user1.savingsb);
+		if (uchoice == 4) //if they choose to exit, break the while loop here before it asks if they want to run again
+			break;
 
 		//checking if user wants to run again
 		printf("\nDo you want to complete another transaction? (y/n): ");
@@ -77,7 +109,7 @@ int main(void) {
 	}
 	
 	//after user ends script
-	printf("\n\n\n");
+	printf("\nThanks for using ENGR0012 Bank\n\n\n");
 	system("pause");
 }
 
@@ -92,5 +124,34 @@ void header(void) {
 
 //function 2 -- menu
 int menu(void) {
-	return(0);
+	int uchoice;
+	printf("\nWhat would you like to do?\n1) Display Balance\n2) Deposit\n3) Withdraw\n4) Exit\n");
+	scanf(" %d", &uchoice);
+	while (uchoice > 4 || uchoice < 1) {
+		printf("\nError! Please enter 1, 2, 3, or 4!\nWhat would you like to do?\n1) Display Balance\n2) Deposit\n3) Withdraw\n4) Exit\n");
+		scanf(" %d", &uchoice);
+	}
+	return(uchoice);
 }
+
+//function 3 -- displaying balance
+void dispbalance(double checkingb, double savingsb) {
+	printf("Checking balance = $%.2lf\nSavings balance = $%.2lf", checkingb, savingsb);
+}
+
+//function 4 -- depositing money
+double deposit(double currentbal) {
+	double depamount,outbal;
+	printf("\nPlease enter the amount you want to deposit: \n");
+	scanf(" %lf", &depamount);
+	while (depamount <= 0) {
+		printf("\nError! Please enter a nonnegative amount you want to deposit: \n");
+		scanf(" %lf", &depamount);
+	}
+	outbal = currentbal + depamount;
+	return(outbal);
+}
+
+
+//function 5 -- withdrawing money
+double withdraw(double currentbal);
