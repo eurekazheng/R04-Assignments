ec5d2b9575a0b3f487a892a939db6bebedfaf1c6
etw10 <etw10@pitt.edu>
2018-04-12 18:12:36 -0400

    kms

--- Assignment 16/Assignment 16/R04_Assignment16_2pm.cpp
+++ Assignment 16/Assignment 16/R04_Assignment16_2pm.cpp
@@ -1,13 +1,38 @@
-#define CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
-
+void matrixinput(int *rows, int *cols, float [][20]);
 
 int main(void) {
+	float matrix1[20][20], matrix2[20][20];
+	int rows1, cols1, rows2, cols2;
 
+	matrixinput(&rows1,&cols1,matrix1);
 
 	system("pause");
 	return(0);
+}
+
+void matrixinput(int *rows, int *cols, float matrix[][20]) {
+	char input[20];
+	FILE *filename;
+	do {
+		printf("Enter the name of your file: ");
+		scanf(" %s", &input);
+	} while (fopen(input, "r") == 0);
+	filename = fopen(input, "r");
+	
+	float temp;
+	fscanf(filename, " %d %d", &rows, &cols);
+	printf("rows = %d cols = %d", rows, cols);
+	scanf("%f", &temp);
+
+	for (int n = 0; n < *rows; n++) {
+		for (int i = 0; i < *cols; i++) {
+			fscanf(filename, " %f", &matrix[n][i]);
+		}
+	}
 }
