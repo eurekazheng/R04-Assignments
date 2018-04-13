d2a918984a7e877a02a946ed25a136f04f5e492e
etw10 <etw10@pitt.edu>
2018-04-12 21:33:56 -0400

    function 1

--- Assignment 16/Assignment 16/R04_Assignment16_2pm.cpp
+++ Assignment 16/Assignment 16/R04_Assignment16_2pm.cpp
@@ -4,35 +4,41 @@
 #include <math.h>
 #include <string.h>
 
-void matrixinput(int *rows, int *cols, float [][20]);
+void matrixinput(int *rows, int *cols, float matrix[][20]);
 
 int main(void) {
 	float matrix1[20][20], matrix2[20][20];
 	int rows1, cols1, rows2, cols2;
 
-	matrixinput(&rows1,&cols1,matrix1);
-
+	matrixinput(&rows1, &cols1, matrix1);
+	matrixinput(&rows2, &cols2, matrix2);
+	
 	system("pause");
 	return(0);
 }
 
 void matrixinput(int *rows, int *cols, float matrix[][20]) {
 	char input[20];
-	FILE *filename;
+	FILE *data;
 	do {
 		printf("Enter the name of your file: ");
 		scanf(" %s", &input);
 	} while (fopen(input, "r") == 0);
-	filename = fopen(input, "r");
-	
-	float temp;
-	fscanf(filename, " %d %d", &rows, &cols);
-	printf("rows = %d cols = %d", rows, cols);
-	scanf("%f", &temp);
 
-	for (int n = 0; n < *rows; n++) {
-		for (int i = 0; i < *cols; i++) {
-			fscanf(filename, " %f", &matrix[n][i]);
+	data = fopen(input, "r");
+	fscanf(data, " %d %d", rows, cols);
+	int check;
+	for (int i = 0; i < *rows; i++) {
+		for (int n = 0; n < *cols; n++) {
+			check = fscanf(data, "%f", &matrix[i][n]);
 		}
 	}
+	for (int i = 0; i < *rows; i++) {
+		printf("\n");
+		for (int n = 0; n < *cols; n++) {
+			printf("%f ", matrix[i][n]);
+		}
+	}
+
+
 }
